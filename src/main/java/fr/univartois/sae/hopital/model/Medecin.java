569c290b5e9b6bc63039d092fb86ed93a36a5bf6
Carl Kolodziejski <carl.kolodziejski19@gmail.com>
2024-05-31 12:44:29 +0200

    Création de la classe Medecin

--- src/main/java/fr/univartois/sae/hopital/model/Medecin.java
+++ src/main/java/fr/univartois/sae/hopital/model/Medecin.java
@@ -1,45 +1,23 @@
 package fr.univartois.sae.hopital.model;
 
 public class Medecin extends Personne {
-    private final int NOMBRE_MAX_RENDEZVOUS = 30;
     private String specialisation;
-    private RendezVous[] listeRendezVous;
-    private int nombreRendezVous;
 
-    public Medecin(String nom, String prenom, String numTel, int age, String specialisation) {
-        super(nom, prenom, numTel, age);
+    public Medecin(String id, String nom, String specialisation) {
+        super(id, nom);
         this.specialisation = specialisation;
-        listeRendezVous = new RendezVous[NOMBRE_MAX_RENDEZVOUS];
-        nombreRendezVous = 0;
     }
 
     public String getSpecialisation() {
         return specialisation;
     }
 
+    public void setSpecialisation(String specialisation) {
+        this.specialisation = specialisation;
+    }
+
     @Override
     public String toString() {
-        return getNom() + " " + getPrenom() + " : " + afficherAge() + ", " + specialisation + ", " + getNumTel();
+        return "Dr. " + getNom() + " : " + getSpecialisation();
     }
-
-    public int getNOMBRE_MAX_RENDEZVOUS() {
-        return NOMBRE_MAX_RENDEZVOUS;
-    }
-
-    public RendezVous[] getListeRendezVous() {
-        return listeRendezVous;
-    }
-
-    public int getNombreRendezVous() {
-        return nombreRendezVous;
-    }
-
-    public void incrementerNombreRendezVous() {
-        nombreRendezVous++;
-    }
-
-    public void decrementerNombreRendezVous() {
-        nombreRendezVous--;
-    }
-
 }
