ce8d897129cd91c7e3a8c9e31bcdd8b156194856
Carl Kolodziejski <carl.kolodziejski19@gmail.com>
2024-06-02 02:36:46 +0200

    Ajout de commentaires pour la classe Medecin

--- src/main/java/fr/univartois/sae/hopital/model/Medecin.java
+++ src/main/java/fr/univartois/sae/hopital/model/Medecin.java
@@ -1,21 +1,46 @@
 package fr.univartois.sae.hopital.model;
 
 public class Medecin extends Personne {
+    /**
+     * La spécialisation du médecin.
+     */
     private String specialisation;
 
+    /**
+     * Le constructeur de la classe Medecin.
+     *
+     * @param id             L'identifiant du médecin.
+     * @param nom            Le nom du médecin.
+     * @param specialisation La spécialisation du médecin.
+     */
     public Medecin(String id, String nom, String specialisation) {
         super(id, nom);
         this.specialisation = specialisation;
     }
 
+    /**
+     * Retourne la spécialisation du médecin.
+     *
+     * @return La spécialisation du médecin.
+     */
     public String getSpecialisation() {
         return specialisation;
     }
 
+    /**
+     * Modifie la spécialisation du médecin.
+     *
+     * @param specialisation La nouvelle spécialisation du médecin.
+     */
     public void setSpecialisation(String specialisation) {
         this.specialisation = specialisation;
     }
 
+    /**
+     * Retourne une représentation en chaîne du médecin.
+     *
+     * @return Une chaîne représentant le médecin.
+     */
     @Override
     public String toString() {
         return "Dr. " + getNom() + " : " + getSpecialisation();
