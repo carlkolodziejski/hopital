64148d1301c2f3688557b683e4fa0430d95e4bac
Carl Kolodziejski <carl.kolodziejski19@gmail.com>
2024-06-02 02:33:04 +0200

    Ajout de commentaires pour la classe Medicament

--- src/main/java/fr/univartois/sae/hopital/model/Medicament.java
+++ src/main/java/fr/univartois/sae/hopital/model/Medicament.java
@@ -1,29 +1,67 @@
 package fr.univartois.sae.hopital.model;
 
 public class Medicament {
+    /**
+     * Le nom du médicament.
+     */
     private String nom;
+
+    /**
+     * Le nombre de jours pendant lequel le médicament doit être pris.
+     */
     private int nombreJours;
+
+    /**
+     * La posologie (fréquence de prise) du médicament.
+     */
     private int posologie;
 
+    /**
+     * Le constructeur du Medicament.
+     *
+     * @param nom         Le nom du médicament.
+     * @param nombreJours Le nombre de jours pendant lequel le médicament doit être pris.
+     * @param posologie   La posologie (fréquence de prise) du médicament.
+     */
     public Medicament(String nom, int nombreJours, int posologie) {
         this.nom = nom;
         this.nombreJours = nombreJours;
         this.posologie = posologie;
     }
 
+    /**
+     * Retourne une représentation en chaîne du médicament.
+     *
+     * @return une chaîne représentant le médicament.
+     */
     @Override
     public String toString() {
         return nom + " : " + nombreJours + ", " + posologie + " par jour.";
     }
 
+    /**
+     * Retourne le nom du médicament.
+     *
+     * @return Le nom du médicament.
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Retourne le nombre de jours pendant lequel le médicament doit être pris.
+     *
+     * @return Le nombre de jours pendant lequel le médicament doit être pris.
+     */
     public int getNombreJours() {
         return nombreJours;
     }
 
+    /**
+     * Retourne la posologie du médicament.
+     *
+     * @return La posologie du médicament.
+     */
     public int getPosologie() {
         return posologie;
     }
