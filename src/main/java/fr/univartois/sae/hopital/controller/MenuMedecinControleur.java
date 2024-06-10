a02522aa10e385b9efc30da5f81008de6d0ce50b
Carl Kolodziejski <carl.kolodziejski19@gmail.com>
2024-06-10 19:10:33 +0200

    Finalisation du menu médecin

---
+++ src/main/java/fr/univartois/sae/hopital/controller/MenuMedecinControleur.java
@@ -0,0 +1,119 @@
+package fr.univartois.sae.hopital.controller;
+
+import fr.univartois.sae.hopital.model.Hopital;
+import fr.univartois.sae.hopital.model.IHopitalControleur;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Classe qui représente le contrôleur du menu médecin.
+ */
+public class MenuMedecinControleur implements IHopitalControleur {
+    /**
+     * L'hôpital controlé par l'application.
+     */
+    private Hopital hopital;
+
+    /**
+     * La fenêtre de l'application.
+     */
+    private Stage stage;
+
+    /**
+     * La scène précédente.
+     */
+    private Scene scenePrecedente;
+
+    /**
+     * Composant représentant le bouton d'ajout de médecin.
+     */
+    @FXML
+    private Button boutonAfficherMedecin;
+
+    /**
+     * Permet de définir l'hôpital contrôlé.
+     *
+     * @param hopital L'hôpital contrôlé.
+     */
+    @Override
+    public void setHopital(Hopital hopital) {
+        this.hopital = hopital;
+
+        boutonAfficherMedecin.disableProperty().bind(hopital.getNbMedecins().isEqualTo(0));
+    }
+
+    /**
+     * Permet de définir la fenêtre de l'application.
+     *
+     * @param stage La fenêtre de l'application.
+     */
+    @Override
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    /**
+     * Permet de définir la scène précédente.
+     *
+     * @param scenePrecedente
+     */
+    @Override
+    public void setScenePrecedente(Scene scenePrecedente) {
+        this.scenePrecedente = scenePrecedente;
+    }
+
+    /**
+     * Renvoie l'utilisateur sur le menu d'affichages des médecins.
+     *
+     * @throws IOException
+     */
+    @FXML
+    void onAfficherMedecinButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/vue-medecin.fxml"));
+        Parent viewContent = fxmlLoader.load();
+
+        MenuAfficherMedecinControleur menuAfficherMedecinControleur = fxmlLoader.getController();
+        menuAfficherMedecinControleur.setStage(stage);
+        menuAfficherMedecinControleur.setHopital(hopital);
+        menuAfficherMedecinControleur.setScenePrecedente(stage.getScene());
+
+        Scene scene = new Scene(viewContent);
+        stage.setScene(scene);
+    }
+
+    /**
+     * Renvoie l'utilisateur sur le menu d'ajout de médecin.
+     *
+     * @throws IOException
+     */
+    @FXML
+    void onAjouterMedecinButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/vue-menu-ajout-medecin.fxml"));
+        Parent viewContent = fxmlLoader.load();
+
+        MenuAjouterMedecinControleur menuAjoutMedecinControleur = fxmlLoader.getController();
+        menuAjoutMedecinControleur.setStage(stage);
+        menuAjoutMedecinControleur.setScenePrecedente(this.stage.getScene());
+        menuAjoutMedecinControleur.setHopital(hopital);
+
+        Scene scene = new Scene(viewContent);
+        stage.setScene(scene);
+    }
+
+    /**
+     * Renvoie l'utilisateur sur le menu principal.
+     */
+    @FXML
+    void onRetourButtonClick() {
+        stage.setScene(scenePrecedente);
+    }
+
+
+}
