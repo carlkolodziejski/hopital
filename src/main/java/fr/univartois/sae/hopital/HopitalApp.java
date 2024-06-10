fe1c0a39ff971b6a16cb92cb8032c204147fdd07
Carl Kolodziejski <carl.kolodziejski19@gmail.com>
2024-06-10 17:15:37 +0200

    Ajout de la classe principale

---
+++ src/main/java/fr/univartois/sae/hopital/HopitalApp.java
@@ -0,0 +1,32 @@
+package fr.univartois.sae.hopital;
+
+import fr.univartois.sae.hopital.controller.MenuPrincipalControleur;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HopitalApp extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("./view/vue-menu-principal.fxml"));
+        Parent viewContent = fxmlLoader.load();
+
+        MenuPrincipalControleur controleur = fxmlLoader.getController();
+        controleur.setStage(stage);
+
+        Scene scene = new Scene(viewContent, 1280, 720);
+        stage.setScene(scene);
+
+        stage.setTitle("Mon hôpital");
+
+        stage.show();
+    }
+}
