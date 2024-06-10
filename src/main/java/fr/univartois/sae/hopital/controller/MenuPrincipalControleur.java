ff4b25792cd2ff47912c24737126b2dd737b4361
Carl Kolodziejski <carl.kolodziejski19@gmail.com>
2024-06-10 17:55:35 +0200

    Remplissage des méthodes permettant de changer de fenêtre

--- src/main/java/fr/univartois/sae/hopital/controller/MenuPrincipalControleur.java
+++ src/main/java/fr/univartois/sae/hopital/controller/MenuPrincipalControleur.java
@@ -4,8 +4,13 @@ import fr.univartois.sae.hopital.model.Hopital;
 import fr.univartois.sae.hopital.model.IHopitalControleur;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MenuPrincipalControleur implements IHopitalControleur {
 
@@ -17,18 +22,45 @@ public class MenuPrincipalControleur implements IHopitalControleur {
     private Stage stage;
 
     @FXML
-    void onChercherButtonClick(ActionEvent event) {
+    void onChercherButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/vue-menu-recherche.fxml"));
+        Parent viewContent = fxmlLoader.load();
 
+        MenuRechercheControleur menuRechercheControleur = fxmlLoader.getController();
+        menuRechercheControleur.setStage(stage);
+        menuRechercheControleur.setScene(this.stage.getScene());
+        menuRechercheControleur.setHopital(hopital);
+
+        Scene scene = new Scene(viewContent);
+        stage.setScene(scene);
     }
 
     @FXML
-    void onMedecinButtonClick(ActionEvent event) {
+    void onMedecinButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/vue-menu-medecin.fxml"));
+        Parent viewContent = fxmlLoader.load();
 
+        MenuMedecinControleur menuMedecinControleur = fxmlLoader.getController();
+        menuMedecinControleur.setStage(stage);
+        menuMedecinControleur.setScene(this.stage.getScene());
+        menuMedecinControleur.setHopital(hopital);
+
+        Scene scene = new Scene(viewContent);
+        stage.setScene(scene);
     }
 
     @FXML
-    void onPatientButtonClick(ActionEvent event) {
+    void onPatientButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/vue-menu-patient.fxml"));
+        Parent viewContent = fxmlLoader.load();
 
+        MenuPatientControleur menuPatientControleur = fxmlLoader.getController();
+        menuPatientControleur.setStage(stage);
+        menuPatientControleur.setScene(this.stage.getScene());
+        menuPatientControleur.setHopital(hopital);
+
+        Scene scene = new Scene(viewContent);
+        stage.setScene(scene);
     }
 
     public void setHopital(Hopital hopital) {
