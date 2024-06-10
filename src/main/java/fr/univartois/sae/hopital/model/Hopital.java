202d081007ac489cdcae58eecb4c77861d40085e
Carl Kolodziejski <carl.kolodziejski19@gmail.com>
2024-06-10 15:25:45 +0200

    Création de la classe Hopital

--- src/main/java/fr/univartois/sae/hopital/model/Hopital.java
+++ src/main/java/fr/univartois/sae/hopital/model/Hopital.java
@@ -35,4 +35,34 @@ public class Hopital {
         }
         return sb.toString();
     }
+
+    public List<Medecin> rechercherParSpecialisation(String specialisation) {
+        List<Medecin> medecinsTrouves = new LinkedList<>();
+        for (Medecin medecin : medecins) {
+            if (medecin.getSpecialisation().equals(specialisation)) {
+                medecinsTrouves.add(medecin);
+            }
+        }
+        return medecinsTrouves;
+    }
+
+    public List<Personne> rechercherParNom(String nom) {
+        List<Personne> personnesTrouvees = new LinkedList<>();
+        for (Medecin medecin : medecins) {
+            if (medecin.getNom().equals(nom)) {
+                personnesTrouvees.add(medecin);
+            }
+        }
+        for (Patient patient : patients) {
+            if (patient.getNom().equals(nom)) {
+                personnesTrouvees.add(patient);
+            }
+        }
+        return personnesTrouvees;
+    }
+
+    public List<Personne> rechercheGenerale() {
+        // TODO Trouver comment implémenter cette méthode.
+        return java.util.Collections.emptyList();
+    }
 }
