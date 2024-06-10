8f0925259591c0f9ea8f96d4327418cb999f8c6f
Carl Kolodziejski <carl.kolodziejski19@gmail.com>
2024-06-10 17:14:03 +0200

    Mise à jour de l'interface

---
+++ src/main/java/fr/univartois/sae/hopital/model/IHopitalControleur.java
@@ -0,0 +1,9 @@
+package fr.univartois.sae.hopital.model;
+
+import javafx.stage.Stage;
+
+public interface IHopitalControleur {
+    void setHopital(Hopital hopital);
+
+    void setStage(Stage stage);
+}
