f2c212ad0d0a0f16bf2cd3c184a431061f480a18
Clément GOUMZI <clementgoumzi62143@gmail.com>
2024-06-11 10:50:56 +0200

    Ajout du test Ordonnance

--- src/test/java/OrdonnanceTest.java
+++ src/test/java/OrdonnanceTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 public class OrdonnanceTest {
 
   @Test
