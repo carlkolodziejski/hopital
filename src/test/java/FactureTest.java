7a888cac22ae5f541dab7736ce428632f2093555
Jus2papaille <matheo.maroille@gmail.com>
2024-06-11 10:51:01 +0200

    test les factures donne les detail donne le statut des paiement et calcul le cout total

--- src/test/java/FactureTest.java
+++ src/test/java/FactureTest.java
@@ -8,10 +8,33 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FactureTest {
 
     @Test
-    @DisplayName("tester calculerCoutTotal")
+    @DisplayName("devraitCalculerCoutTotal")
     public void testCalculerCoutTotal() {
         Facture facture = new Facture("1", "Facture de test", false);
         facture.calculerCoutTotal();
-        assertEquals(150.0, facture.getCoutTotal());
+        assertEquals(0, facture.getCoutTotal());
     }
+
+    @Test
+    @DisplayName("devraitRetournerDetails")
+    public void devraitRetournerDetails() {
+        Facture facture = new Facture("1", "details", false);
+        assertEquals("details", facture.getDetails());
+    }
+
+    @Test
+    @DisplayName("devraitRetournerStatutPaiement")
+    public void devraitRetournerStatutPaiement() {
+        Facture facture = new Facture("1", "details", false);
+        assertFalse(facture.isPayee());
+    }
+
+    @Test
+    @DisplayName("devraitChangerStatutPaiement")
+    public void devraitChangerStatutPaiement() {
+        Facture facture = new Facture("1", "details", false);
+        facture.setPayee(true);
+        assertTrue(facture.isPayee());
+    }
+
 }
