2d5fe643e24cc92fba63c258778e480c57995dc6
Jus2papaille <matheo.maroille@gmail.com>
2024-06-11 11:55:21 +0200

    amelioration de rendezVousTest pour atteindre 100% du test

--- src/test/java/RendezVousTest.java
+++ src/test/java/RendezVousTest.java
@@ -4,29 +4,54 @@ import fr.univartois.sae.hopital.model.Patient;
 import fr.univartois.sae.hopital.model.RendezVous;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class RendezVousTest {
 
     @Test
     @DisplayName("devraitEnregistrerVisite")
     public void devraitEnregistrerVisite() {
-        // Création des mocks
         Patient patient = mock(Patient.class);
         HistoriqueMedical historique = mock(HistoriqueMedical.class);
         when(patient.getHistoriqueMedical()).thenReturn(historique);
 
         Medecin medecin = mock(Medecin.class);
 
-        // Création de l'objet à tester
         RendezVous rendezVous = new RendezVous("1", "Consultation", 50.0, patient, medecin, LocalDateTime.now());
 
-        // Appel de la méthode à tester
         rendezVous.enregistrerVisite();
 
-        // Vérification que la méthode ajouterRendezVous a été appelée sur l'historique médical
         verify(historique, times(1)).ajouterRendezVous(rendezVous);
+    }
+
+    @Test
+    @DisplayName("devraitRetournerPrix")
+    public void devraitRetournerPrix() {
+        Patient patient = mock(Patient.class);
+        Medecin medecin = mock(Medecin.class);
+
+        RendezVous rendezVous = new RendezVous("1", "Consultation", 50.0, patient, medecin, LocalDateTime.now());
+
+        assertEquals(50.0, rendezVous.getPrix());
+    }
+
+    @Test
+    @DisplayName("devraitRetournerRepresentationChaine")
+    public void devraitRetournerRepresentationChaine() {
+        Patient patient = mock(Patient.class);
+        when(patient.getNom()).thenReturn("Doe");
+
+        Medecin medecin = mock(Medecin.class);
+        when(medecin.getNom()).thenReturn("Smith");
+
+        LocalDateTime dateHeure = LocalDateTime.of(2022, 1, 1, 10, 0);
+        RendezVous rendezVous = new RendezVous("1", "Consultation", 50.0, patient, medecin, dateHeure);
+
+        String expected = "Rendez-vous du 1/1/2022 à 10h0 : Consultation avec le Dr.Smith pour Doe";
+        assertEquals(expected, rendezVous.toString());
     }
 }
